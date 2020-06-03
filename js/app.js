e545c72b8df93c71359b799aa92cb03731e3f3e6
zealptl <zealpatel06@gmail.com>
2020-06-02 23:11:09 -0400

    Added how-to popup modal

--- js/app.js
+++ js/app.js
@@ -1,24 +1,10 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("howToModal");
+let btn = document.getElementById("howToBtn");
+let closeBtn = document.getElementById("closeBtn");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+btn.onclick = () => (modal.style.display = "block");
+closeBtn.onclick = () => (modal.style.display = "none");
+window.onclick = (event) => {
   if (event.target == modal) {
     modal.style.display = "none";
   }
